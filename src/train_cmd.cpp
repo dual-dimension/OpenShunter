74b07ac8fa5a033afa5111f4be22021fb3cf1ed7
Ian H <ianhenderson1@live.co.uk>
2026-05-21 03:29:44 +0100

    Missing file

--- src/train_cmd.cpp
+++ src/train_cmd.cpp
@@ -3134,10 +3134,10 @@ static void TrainEnterStation(Train *consist, StationID station)
  */
 static inline bool CheckCompatibleRail(const Train *v, TileIndex tile, bool check_railtype)
 {
-	return OpenShunter::AskCanTrainEnterTile(static_cast<uint32_t>(tile), GetTileOwner(tile).base(), v->owner.base(), IsTileOwner(tile, v->owner)) &&
+    bool result = IsTileOwner(tile, v->owner) &&
 			(!check_railtype || !v->IsFrontEngine() || v->compatible_railtypes.Test(GetRailType(tile)));
 
-	return Shunter::AskCanTrainEnterTile(tile.base(), GetTileOwner(tile).base(), v->owner.base(), result);
+	return OpenShunter::AskCanTrainEnterTile(static_cast<uint32_t>(tile), GetTileOwner(tile).base(), v->owner.base(), result);
 }
 
 /** Data structure for storing engine speed changes of an acceleration type. */
