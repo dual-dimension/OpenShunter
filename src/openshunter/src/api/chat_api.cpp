3bcc0ff27e160a80b71376b5a68a2d600e4d151e
Ian H <ianhenderson1@live.co.uk>
2026-05-21 17:33:14 +0100

    Compile errors

--- src/openshunter/src/api/chat_api.cpp
+++ src/openshunter/src/api/chat_api.cpp
@@ -7,14 +7,14 @@ void SendChat(const char* message)
 {
     if (_networking && _network_server)
     {
-        NetworkServerSendChat(NETWORK_ACTION_CHAT, DESTTYPE_BROADCAST, 0, message, CLIENT_ID_SERVER);
+        NetworkServerSendChat(NetworkAction::ChatBroadcast, NetworkChatDestinationType::Broadcast, 0, message, CLIENT_ID_SERVER);
     }
     else if (_networking)
     {
-        NetworkClientSendChat(NETWORK_ACTION_CHAT, DESTTYPE_BROADCAST, 0, message);
+        NetworkClientSendChat(NetworkAction::ChatBroadcast, NetworkChatDestinationType::Broadcast, 0, message);
     }
     else
     {
-        NetworkTextMessage(NETWORK_ACTION_CHAT, TC_WHITE, false, "Mod", message);
+        NetworkTextMessage(NetworkAction::ChatBroadcast, CC_DEFAULT, false, "Mod", message);
     }
 }
